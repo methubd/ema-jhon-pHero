3b3b009140b94fc165b2e3d7317ec9b747364dc2
T R Methu <fmethubd@gmail.com>
2023-04-05 11:28:36 +0600

    implemented delete from cart

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import './Orders.css'
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData();
-    // console.log(products);
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart)
+    
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+
     return (
         <div className='shop-container'>
-            <div className='products-container'>
-                <h2>Orders Page: {cart.length}</h2>
+            <div className='review-container'>
+                {
+                    cart.map(product => <ReviewItem
+                        key = {product.id}
+                        product={product}
+                        handleRemoveFromCart = {handleRemoveFromCart}
+                    ></ReviewItem>)
+                }
             </div>
             <div className='cart-container'>
                 <Cart cart = {cart}></Cart>
