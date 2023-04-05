a66aafaf8ae29586cd1b978ea705d138c0224e71
T R Methu <fmethubd@gmail.com>
2023-04-05 12:02:07 +0600

    process check out added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Cart.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Cart = (props) => {
     // const cart = props.cart; //option 1
-    const {cart} = props;
+    const {cart, handleClearCart, children} = props;
 
     let total = 0;
     let totalShipping = 0;
@@ -30,6 +32,11 @@ const Cart = (props) => {
             <p>Total Shipping:${totalShipping} </p>
             <p>Tax: {tax.toFixed(2)} </p>
             <h6>Grand Total: {grandTotal.toFixed(2)} </h6>
+            <button onClick={handleClearCart} className='btn-clear-cart'>
+                <span>Clear Cart</span>
+            <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
+            {children}
         </div>
     );
 };
